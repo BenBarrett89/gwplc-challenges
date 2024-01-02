d2167fa35e7007556d169e2cd2ec9d6fa2c430a2
Ben Barrett <benjaminbarrett1989@gmail.com>
2024-01-02 09:43:26 +0000

    test: add test scenarios for numeralToNumber
    Also install jest types and run npm audit fix

--- coding/english-numerals-main/numeralToNumber.test.ts
+++ coding/english-numerals-main/numeralToNumber.test.ts
@@ -1,7 +1,40 @@
 import { numeralToNumber } from "./numeralToNumber";
 
-test("Should return a number", () => {
-  const result = numeralToNumber("");
-
-  expect(typeof result).toBe("number");
+describe("numeralToNumber", () => {
+  it('should return single digit numbers when provided with their English equivalents (lower case)', () => {
+    const result = numeralToNumber("three");
+    expect(result).toBe(3);
+  })
+  it('should return single digit numbers when provided with their English equivalents (capitalized)', () => {
+    const result = numeralToNumber("Three");
+    expect(result).toBe(3);
+  })
+  it('should return double digit numbers when provided with their English equivalents (single words)', () => {
+    const result = numeralToNumber("Twelve");
+    expect(result).toBe(12);
+  })
+  it('should return double digit numbers when provided with their English equivalents (hyphenated)', () => {
+    const result = numeralToNumber("Twenty-four");
+    expect(result).toBe(24);
+  })
+  it('should return double digit numbers when provided with their English equivalents (space separated)', () => {
+    const result = numeralToNumber("twenty four");
+    expect(result).toBe(24);
+  })
+  it('should return three digit numbers when provided with their English equivalents (with other numbers)', () => {
+    const result = numeralToNumber("Eight hundred and twelve");
+    expect(result).toBe(812);
+  })
+  it('should return six digit numbers when provided with their English equivalents', () => {
+    const result = numeralToNumber("Seven hundred and forty nine thousand, five hundred and eighty one");
+    expect(result).toBe(749581);
+  })
+  it('should return 1 million when provided with one million (maximum)', () => {
+    const result = numeralToNumber("One million");
+    expect(result).toBe(1000000);
+  })
+  it('should return NaN when not provided with a valid number in English', () => {
+    const result = numeralToNumber("This is not a number");
+    expect(result).toBe(NaN);
+  })
 });
